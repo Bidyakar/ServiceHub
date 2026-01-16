1c417edad0c635dcef5ead8fd221d34981e492aa
Bidyakar Bhatta <bhattbidyakar@gmail.com>
2026-01-16 23:03:01 +0545

    updated root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
-"use client";
-
 // app/layout.tsx
-import Navbar from '@/components/common/Navbar';
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
 import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen flex flex-col">
         <Navbar />
-        {children}
+
+        {/* Page content */}
+        <main className="flex-1">
+          {children}
+        </main>
+
+        {/* Footer ALWAYS visible */}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
